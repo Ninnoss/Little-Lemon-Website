476a820d47ad3c7d0fd9a2d73936c6a061b58688
Ninos <ninos.dinkha97@gmail.com>
2023-07-17 17:17:18 +0300

    used react-player and added video autoplay

--- src/components/HomePage/RestaurantVideo.jsx
+++ src/components/HomePage/RestaurantVideo.jsx
@@ -1,42 +1,42 @@
-import { useState, useRef } from 'react';
+import { useState, useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
+import ReactPlayer from 'react-player/file';
 import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
 import images from '../../data/images';
 
 const RestaurantVideo = () => {
   const [playVideo, setPlayVideo] = useState(false);
-  const videoRef = useRef(null);
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.5,
+  });
+
+  useEffect(() => {
+    setPlayVideo(inView);
+  }, [inView]);
 
   const handleVideoToggle = () => {
-    const videoElement = videoRef.current;
-
-    if (videoElement.paused) {
-      videoElement.play();
-    } else {
-      videoElement.pause();
-    }
-
-    setPlayVideo(!videoElement.paused);
+    setPlayVideo(!playVideo);
   };
 
   return (
     <div
-      onClick={handleVideoToggle}
+      ref={ref}
       className="relative overlay">
-      <video
-        ref={videoRef}
-        loop
-        muted
-        // autoPlay={true}
-        playsInline={true}
+      <ReactPlayer
+        url={images.mealVideo}
+        playing={playVideo}
+        type="video/mp4"
+        loop={true}
+        muted={true}
         width="100%"
-        height="100%">
-        <source
-          src={images.mealVideo}
-          type="video/mp4"
-        />
-      </video>
+        height="100%"
+        playsinline={true}
+      />
       <div className="absolute inset-0 flex items-center justify-center">
-        <div className="w-20 h-20 rounded-full border-2 border-yellow-400 flex items-center justify-center cursor-pointer">
+        <div
+          className="w-20 h-20 rounded-full border-2 border-yellow-400 flex items-center justify-center cursor-pointer"
+          onClick={handleVideoToggle}>
           {playVideo ? (
             <BsPauseFill
               color="#fff"
@@ -55,49 +55,3 @@ const RestaurantVideo = () => {
 };
 
 export default RestaurantVideo;
-
-// import { useState } from 'react';
-// import ReactPlayer from 'react-player/file';
-// import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
-// import images from '../../data/images';
-
-// const RestaurantVideo = () => {
-//   const [playVideo, setPlayVideo] = useState(false);
-
-//   const handleVideoToggle = () => {
-//     setPlayVideo(!playVideo);
-//   };
-
-//   return (
-//     <div className="relative overlay">
-//       <ReactPlayer
-//         url={images.mealVideo}
-//         playing={playVideo}
-//         type="video/mp4"
-//         loop={true}
-//         muted={true}
-//         width="100%"
-//         height="100%"
-//       />
-//       <div className="absolute inset-0 flex items-center justify-center">
-//         <div
-//           className="w-20 h-20 rounded-full border-2 border-yellow-400 flex items-center justify-center cursor-pointer"
-//           onClick={handleVideoToggle}>
-//           {playVideo ? (
-//             <BsPauseFill
-//               color="#fff"
-//               fontSize={30}
-//             />
-//           ) : (
-//             <BsFillPlayFill
-//               color="#fff"
-//               fontSize={30}
-//             />
-//           )}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default RestaurantVideo;
